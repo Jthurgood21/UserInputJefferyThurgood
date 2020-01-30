e1309d742a8886c63a4507cdb8b3e1f5a0476d93
Jeffery Thurgood <jthurgood@usf.edu>
2020-01-30 15:33:37 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UserInputJefferyThurgood
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hi, this is Jeffery Thurgood!");
+        }
+    }
+}
