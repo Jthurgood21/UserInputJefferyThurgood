3e002118096d22861b52c980224897d3831fe065
Jeffery Thurgood <jthurgood@usf.edu>
2020-01-30 16:04:12 -0500

    Applied code for the reading and output of user feedback

--- Program.cs
+++ Program.cs
@@ -7,6 +7,10 @@ namespace UserInputJefferyThurgood
         static void Main(string[] args)
         {
             Console.WriteLine("Hi, this is Jeffery Thurgood!");
+            string UserFeedback;
+            Console.WriteLine("What can I do to help?"); 
+            UserFeedback=Console.ReadLine();
+            Console.WriteLine("You typed:" + UserFeedback);
         }
     }
 }
